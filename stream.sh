4f9f8f39cd0e1291d549e19af868eca50e743305
ZEN <dmytro.poltavchenko@gmail.com>
2022-02-05 02:17:21 +0200

    Invoke ffmpeg in background to fill up mbuffer

--- stream.sh
+++ stream.sh
@@ -1,30 +1,48 @@
 #!/usr/bin/env bash
 
+set --;
 trap 'test -e "$TITLE_FILE" && rm "$TITLE_FILE"; test -e "$TMP_FILE" && rm "$TMP_FILE"' EXIT;
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 TITLE_FILE="$(mktemp)";
 TMP_FILE="$(mktemp)";
 VIDEO="$DIR/cassete.gif";
-MUSIC_FOLDER="$DIR/music";
+MUSIC_DIR="$DIR/music";
 RTMP_SERVERS="[f=flv]rtmp://142.250.184.206/live/radio?access_token=84VzGMZuUrmftDCvnr";
 
-which mbuffer 2>/dev/null || { echo "[ERROR] mbuffer not found" 1>&2; exit 1; }
-which ffmpeg 2>/dev/null || { echo "[ERROR] ffmpeg not found" 1>&2; exit 1; }
-which jq 2>/dev/null || { echo "[ERROR] jq not found" 1>&2; exit 1; }
+which mbuffer &>/dev/null || { echo "[ERROR] mbuffer not found" 1>&2; exit 1; }
+which ffmpeg &>/dev/null || { echo "[ERROR] ffmpeg not found" 1>&2; exit 1; }
+which jq &>/dev/null || { echo "[ERROR] jq not found" 1>&2; exit 1; }
+which ps &>/dev/null || { echo "[ERROR] ps not found" 1>&2; exit 1; }
 
 while true; do
-    MP3_FILE="$(find "$MUSIC_FOLDER" -type f -name '*.mp3' -not -path 'Favorite' | shuf -n 1)";
-    ffprobe -show_format -print_format json "$MP3_FILE" 2>/dev/null \
-        | jq -r '.format | [.tags.artist,.tags.title] | join(" - ")' > "$TMP_FILE";
+    EXP_TIME=${1/ */};
+    if test $# -gt 0 -a "$(date +%s)" -ge "$EXP_TIME"; then
+        shift && set -- "$@";
+    fi
+
+    echo ${1#* } > "$TMP_FILE";
     mv "$TMP_FILE" "$TITLE_FILE";
-    ffmpeg \
-        -re \
-        -hide_banner \
-        -i "$MP3_FILE" \
-        -vn \
-        -acodec copy \
-        -f mpegts -;
-done | mbuffer -q -c -m 512k | (
+
+    kill -0 "$(jobs -p | head -n 1)" &> /dev/null && { sleep 1s; continue; } || wait;
+
+    MP3_FILE="$(find "$MUSIC_DIR" -type f -name '*.mp3' | shuf -n 1)";
+
+    SONG_TITLE="$(ffprobe -show_format -print_format json "$MP3_FILE" 2>/dev/null \
+        | jq -r '.format | [.tags.artist,.tags.title] | join(" - ")')";
+    SONG_LEN="$(ffprobe -show_format -print_format json "$MP3_FILE" 2>/dev/null \
+        | jq -r .format.duration | sed 's|\..*||')";
+
+    START_TIME=$(date +%s);
+    if test $# -gt 0; then
+        LAST_QUEUED="${@: -1}";
+        START_TIME=${LAST_QUEUED/ */};
+    fi
+
+    set -- "$@" "$(( $START_TIME + $SONG_LEN )) $SONG_TITLE";
+
+    ffmpeg -hide_banner -i "$MP3_FILE" -vn -acodec copy -f mpegts - &
+done | mbuffer -q -c -m 20000k | (
     ffmpeg -hide_banner \
         -stream_loop -1 -i "$VIDEO" \
         -err_detect explode \
